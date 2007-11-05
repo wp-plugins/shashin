ab683ebe688cf1c928059fcb0aaca7f262118e5a
toppa <toppa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-11-05 12:35:14 +0000

    prep to rename file
    git-svn-id: https://plugins.svn.wordpress.org/shashin/trunk@24084 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ShashinParser.php
+++ ShashinParser.php
@@ -1,44 +1,32 @@
 <?php
 /**
- * ShashinParser class file.
+ * ToppaXMLParser class file.
+ *
+ * This file is part of Shashin. Please see the Shashin.php file for
+ * copyright and license information.
  *
  * @author Michael Toppa
- * @version 1.0.6
+ * @version 1.2
  * @package Shashin
  * @subpackage Classes
- *
- * Copyright 2007 Michael Toppa
- * 
- * This file is part of Shashin.
- *
- * Shashin is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * Shashin is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+ 
 /**
+ * A simple XML parser. Looks for "item" entries in the feed, and builds an
+ * array of items. Captures item values and attributes.
  *
  * @author Michael Toppa
  * @package Shashin
  * @subpackage Classes
  */
-class ShashinParser {
+class ToppaXMLParser {
     var $allTags = array();
     var $insideItem = false;
     var $tag;
     var $parser;
     var $counter = 0;
     
-    function ShashinParser() {
+    function ToppaXMLParser() {
         $this->parser = xml_parser_create();
         xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false);
         xml_set_object($this->parser, $this);
@@ -69,7 +57,7 @@ class ShashinParser {
     }
 
     function startElement($parser, $name, $attrs) {
-    	if ($this->insideItem) {
+    	if ($this->insideItem === true) {
             $this->allTags[$this->counter][$name] = array();
             $this->allTags[$this->counter][$name]['attrs'] = $attrs;
             $this->tag = $name;
@@ -94,7 +82,7 @@ class ShashinParser {
     }
 }
 
-$parser = new ShashinParser();
+$parser = new ToppaXMLParser();
 print "<pre>";
 print_r($parser->parse("http://picasaweb.google.com/data/feed/api/user/michaeltoppa?kind=album&alt=rss&hl=en_US"));
 print "</pre>";
