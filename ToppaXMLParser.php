befd4286c1df3aed8475f46b212770be44d5d4d1
toppa <toppa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-28 01:27:04 +0000

    Shashin 2.1
    git-svn-id: https://plugins.svn.wordpress.org/shashin/tags/2.1@43019 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ToppaXMLParser.php
+++ ToppaXMLParser.php
@@ -6,11 +6,18 @@
  * copyright and license information.
  *
  * @author Michael Toppa
- * @version 1.2
+ * @version 2.1
  * @package Shashin
  * @subpackage Classes
  */
- 
+
+/**
+ * We'll use Snoopy to fetch the RSS feed - this is a safer bet across various
+ * server configurations than fopen
+ */
+require_once(ABSPATH . WPINC . '/class-snoopy.php');
+//require_once('class-snoopy.php');
+
 /**
  * A simple XML parser. Looks for "item" entries in the feed, and builds an
  * array of items. Captures item values and attributes.
@@ -19,41 +26,36 @@
  * @package Shashin
  * @subpackage Classes
  */
+
 class ToppaXMLParser {
     var $allTags = array();
     var $insideItem = false;
     var $tag;
     var $parser;
     var $counter = 0;
-    
+
     function ToppaXMLParser() {
         $this->parser = xml_parser_create();
         xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false);
         xml_set_object($this->parser, $this);
         xml_set_element_handler($this->parser, "startElement", "endElement");
         xml_set_character_data_handler($this->parser, "characterData");
-    }   
-    
+    }
+
     function parse($url) {
-        $fp = fopen($url, "r");
-        
-        if ($fp === false) {
-            return "Failed to open URL: $url";
+        $client = new Snoopy();
+	    $client->fetch($url);
+        $clean = str_replace('&', '%and%', $client->results); // sucky
+        $ret = xml_parse($this->parser, $clean);
+
+        if (!$ret) {
+            return(sprintf("XML error: %s at line %d",
+    			xml_error_string(xml_get_error_code($this->parser)),
+    			xml_get_current_line_number($this->parser)));
         }
-        
-        while ($data = fread($fp, 4096)) {
-        	$ret = xml_parse($this->parser, $data, feof($fp));
-            
-            if (!$ret) {
-                return(sprintf("XML error: %s at line %d", 
-        			xml_error_string(xml_get_error_code($this->parser)), 
-        			xml_get_current_line_number($this->parser)));
-            }
-        }
-        
-        fclose($fp);
+
         xml_parser_free($this->parser);
-        return $this->allTags;    
+        return $this->allTags;
     }
 
     function startElement($parser, $name, $attrs) {
@@ -76,14 +78,18 @@ class ToppaXMLParser {
 
     function characterData($parser, $data) {
     	if ($this->insideItem === true) {
-            $this->allTags[$this->counter][$this->tag]['data'] = $data;
+            $data = str_replace('%and%', '&', $data); // sucky
 
+            // The use of .= here is crucial for two reasons:
+            // 1. the php parser divides the data into 1024 byte chunks, if your
+            //    data falls in the middle of chunk, this function is called
+            //    again, so you need to concatenate the data
+            // 2. entities, newlines, and tabs cause it to stop scanning and
+            //    and call this method again, so you need to concatenate the
+            //    data from each call
+            $this->allTags[$this->counter][$this->tag]['data'] .= $data;
     	}
     }
 }
 
-$parser = new ToppaXMLParser();
-print "<pre>";
-print_r($parser->parse("http://picasaweb.google.com/data/feed/api/user/michaeltoppa?kind=album&alt=rss&hl=en_US"));
-print "</pre>";
 ?>
